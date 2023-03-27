8ed813527a596730abed9f1adc86060da270b8c7
ivenckunas <ignas.venckunas@gmail.com>
2023-03-27 13:43:58 +0300

    udemy intro 81

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,12 +1,42 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 
 const BoxScreen = () => {
 	return (
-		<View>
-			<Text>BoxScreen</Text>
+		<View style={styles.viewStyle}>
+			<View style={styles.viewOneStyle}></View>
+			<View style={styles.viewTwoStyle}></View>
+			<View style={styles.viewThreeStyle}></View>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	viewStyle: {
+		borderWidth: 3,
+		height: 200,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	viewOneStyle: {
+		height: 50,
+		width: 100,
+		borderWidth: 3,
+		borderColor: 'violet',
+	},
+	viewTwoStyle: {
+		height: 50,
+		width: 100,
+		borderWidth: 3,
+		borderColor: 'green',
+		marginTop: 50,
+	},
+	viewThreeStyle: {
+		height: 50,
+		width: 100,
+		borderWidth: 3,
+		borderColor: 'blue',
+	},
+});
 
 export default BoxScreen;
